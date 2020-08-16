984bc44252910f4b740b76062626a0230946c1dd
yelim <720angela98@gmail.com>
2020-08-17 01:03:01 +0900

    commit1

--- app/src/main/java/com/example/creditcalculator/MainActivity.java
+++ app/src/main/java/com/example/creditcalculator/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPause() {
+    protected void onPause() { 
         super.onPause();
         finish();
     }
